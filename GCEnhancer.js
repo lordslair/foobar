243c3e6fa6df8531a6e20cff19f614e854f81583
lordslair <5469390+lordslair@users.noreply.github.com>
2018-07-31 19:47:53 +0200

    Fixed URL

--- GCEnhancer.js
+++ GCEnhancer.js
@@ -2,9 +2,8 @@
 // @name           Gobland Clan Enhancer
 // @namespace      GCEnhancer
 // @description    Replaces part of informations in game to make more visible information related to clan's players, allies, and ennemies.
-// @include        http://*
-// @include        https://*
-// @include        file://*
+// @include        http://games.gobland.fr
+// @include        https://games.gobland.fr
 // @copyright      lordslair
 // @version        0.0.1
 // @downloadURL    
