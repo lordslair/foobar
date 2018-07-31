f8c492233aece4ad7260d93619ff060ef14cbdfd
lordslair <5469390+lordslair@users.noreply.github.com>
2018-08-01 00:11:35 +0200

    Fixed Typo

--- GCEnhancer.js
+++ GCEnhancer.js
@@ -6,7 +6,7 @@
 // @include        https://games.gobland.fr
 // @copyright      lordslair
 // @license        https://creativecommons.org/licenses/by-sa/4.0/
-// @version        0.0.3
+// @version        0.0.4
 // @downloadURL    https://greasyfork.org/scripts/370776-gobland-clan-enhancer/code/Gobland%20Clan%20Enhancer.user.js
 // @updateURL      https://greasyfork.org/scripts/370776-gobland-clan-enhancer/code/Gobland%20Clan%20Enhancer.user.js
 // ==/UserScript==
@@ -21,10 +21,7 @@
 //      '((214))'                                         : '🆘', // Ennemis
     };
 
-    //////////////////////////////////////////////////////////////////////////////
-    // This is where the real code is
-    // Don't edit below this
-    //////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////
 
     var regexs = [], replacements = [],
         tagsWhitelist = ['PRE', 'BLOCKQUOTE', 'CODE', 'INPUT', 'BUTTON', 'TEXTAREA'],
