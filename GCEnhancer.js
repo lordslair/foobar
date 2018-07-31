0b8b16876ed25bf858e8e1f0672fb67ed0b11085
lordslair <5469390+lordslair@users.noreply.github.com>
2018-08-01 00:14:18 +0200

    Updated version

--- GCEnhancer.js
+++ GCEnhancer.js
@@ -6,7 +6,7 @@
 // @include        https://games.gobland.fr
 // @copyright      lordslair
 // @license        https://creativecommons.org/licenses/by-sa/4.0/
-// @version        0.0.4
+// @version        0.0.5
 // @downloadURL    https://raw.githubusercontent.com/lordslair/foobar/master/GCEnhancer.js
 // @updateURL      https://raw.githubusercontent.com/lordslair/foobar/master/GCEnhancer.js
 // ==/UserScript==
