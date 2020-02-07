370a7df4434d82f0cefdf05c77c9fb0db18f67c8
lordslair <rvandepo+github@gmail.com>
2020-02-07 21:18:20 +0100

    Modified MySQL post-install

--- k8s/gl-legacy/mysql/entrypoint.sh
+++ k8s/gl-legacy/mysql/entrypoint.sh
@@ -7,16 +7,12 @@ apt-get update \
     && wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.0.96-linux-x86_64-glibc23.tar.gz -O /tmp/mysql.tar.gz \
     && tar -xzf /tmp/mysql.tar.gz --no-same-owner -C /usr/local/mysql --strip-components=1 \
     && rm -rf /tmp/mysql.tar.gz \
-    && mkdir /etc/mysql  \
-    && mv /usr/local/mysql/my.cnf /etc/my.cnf \
+    && mkdir /etc/mysql \
+    && mv /usr/local/mysql/my.cnf /etc/mysql/my.cnf \
     && useradd --system mysql \
     && chown mysql:mysql /var/lib/mysql \
     && cd /usr/local/mysql \
     && ./scripts/mysql_install_db --datadir=/var/lib/mysql --user=mysql
-
-/usr/local/mysql/support-files/mysql.server start
-/usr/local/mysql/bin/mysql --defaults-file=/etc/my.cnf < /usr/local/mysql/mysql_secure_installation.SQL
-/usr/local/mysql/support-files/mysql.server stop
 
 echo "`date +"%F %X"` Build done ..."
 
