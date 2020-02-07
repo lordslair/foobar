8871e5a685aa2e10390ba94eab292151e4305313
lordslair <rvandepo+github@gmail.com>
2020-02-07 23:04:47 +0100

    Added PHP entrypoint

---
+++ k8s/gl-legacy/php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "`date +"%F %X"` Building PHP dependencies and system set-up ..."
+
+apk update --no-cache \
+    && apk add --no-cache --virtual .build-deps \
+                                    tzdata \
+    && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
+    && apk del .build-deps \
+    && docker-php-ext-install mysql \
+    && docker-php-ext-enable mysql
+
+echo "`date +"%F %X"` Build done ..."
+
+exec php-fpm
